6ee35c78c25545a07ac54b0f2cc1956b326e07a7
tomas-stejskal <tomas.stejskal@akademiasovy.sk>
2017-10-03 20:07:26 +0200

    add local storage support

--- table_script.js
+++ table_script.js
@@ -63,11 +63,14 @@ function remove_rov(cell_id) {
     var t_body = cell_id.parentNode.parentNode.parentNode;
     t_body.removeChild(parent);
     //romove form arr
-    console.log(rov_id);
     for (var i = data.length -1 ; i >= 0; i--) {
         if (data[i].id == rov_id) {
             data[i].is_removed = true;
         }
+    }
+    
+    if (t_body.childNodes.length == 0) {
+        document.getElementById('tabulka').style.display = 'none';
     }
 }
 
@@ -144,4 +147,55 @@ function build_table_by_dob() {
                 return data[j].dob;
         }
     }
+}
+
+function save_data_to_storage() {
+    var to_sav = [];
+    for (var i = 0 ; i < data.length; i++) {
+        if (!data[i].is_removed) {
+            to_sav[to_sav.length] = data[i];
+        }
+    }
+    localStorage.removeItem('_tabulka_');
+    if (to_sav.length > 0) {
+        localStorage.setItem('_tabulka_', JSON.stringify(to_sav));
+    }
+}
+
+function load_data_from_storage() {
+    var s_data = localStorage.getItem('_tabulka_');
+    if (s_data === null)
+        return;
+    s_data = JSON.parse(s_data);
+    for (var i = 0; i < s_data.length; i++) {
+        data[data.length] = s_data[i];
+
+        var table = document.getElementById("telo_tabulky");
+        var riadok = document.createElement("tr");
+        var bunka1 = document.createElement("td");
+        var bunka2 = document.createElement("td");
+        var bunka3 = document.createElement("td");
+        var bunka4 = document.createElement("td");
+
+
+        bunka1.innerHTML = s_data[i].first_name;
+        bunka2.innerHTML = s_data[i].last_name;
+        bunka3.innerHTML = s_data[i].dob;
+        bunka4.innerHTML = '<button onclick="remove_rov(this)" class="btn btn-danger">X</button>';
+        riadok.appendChild(bunka1);
+        riadok.appendChild(bunka2);
+        riadok.appendChild(bunka3);
+        riadok.appendChild(bunka4);
+        riadok.setAttribute('id', "row_" + record_counter);
+        if (s_data[i].gender) {
+            riadok.setAttribute('gender', 'female');
+        } else {
+            riadok.setAttribute('gender', 'male');
+        }
+        table.appendChild(riadok);
+
+        document.getElementById("tabulka").setAttribute("style", "margin:0 auto;");
+        record_counter++;
+    }
+
 }
